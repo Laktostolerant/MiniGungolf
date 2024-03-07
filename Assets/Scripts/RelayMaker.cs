92d838859c60e48cd38de24e4248100bcdf22860
Tiger <tigominecraft@gmail.com>
2024-03-07 20:31:36 +0100

    UI stuff

---
+++ Assets/Scripts/RelayMaker.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Services.Authentication;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using System.Threading.Tasks;
+using Unity.Services.Lobbies;
+//using UnityEditor.PackageManager;
+
+public class RelayMaker : MonoBehaviour
+{
+    public static RelayMaker Instance { get; private set; }
+    private void Awake() { Instance = this; }
+ 
+    [ContextMenu("Create a Relay")]
+    public async Task<string> CreateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            Debug.Log("Join code: " + joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartHost();
+
+            return joinCode;
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+            return null;
+        }
+    }
+
+    public async void JoinRelay(string joinCode)
+    {
+        try
+        {
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartClient();
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    public void LeaveRelay()
+    {
+        try
+        {
+            LobbyManager.Instance.LeaveLobby();
+
+            AuthenticationService.Instance.SignOut();
+            NetworkManager.Singleton.Shutdown();
+        }
+        catch(RelayServiceException e)
+        {
+
+        }
+    }
+}
